97c8bac70078c86257acf74312e19681dfe72ec6
Nazarii7 <nazariipushkaruk@gmail.com>
2022-12-12 00:30:33 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import { useEffect } from 'react';
+import { lazy } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router';
+
+const HomePage = lazy(() => import('../pages/home'));
+const RegisterPage = lazy(() => import('../pages/register'));
+const LoginPage = lazy(() => import('../pages/login'));
+const ContactsPage = lazy(() => import('../pages/contacts'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<></>}>
+        <Route index element={<HomePage />} />
+        <Route path="/register" element={null} />
+        <Route path="/login" element={null} />
+        <Route path="/contacts" element={null} />
+      </Route>
+    </Routes>
   );
 };
